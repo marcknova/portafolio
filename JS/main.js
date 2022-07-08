6756f241d964d31e4dabd19bed95dfd5e3cfe7fb
Marco Antonio <marck.nova@gmail.com>
2022-07-07 23:54:59 -0500

    hamburger menu

--- JS/main.js
+++ JS/main.js
@@ -1,45 +1,60 @@
-window.sr = ScrollReveal();
+// window.sr = ScrollReveal();
 
-sr.reveal('.nav-menu', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '-100px'
+// sr.reveal('.nav-menu', {
+//     duration: 2000,
+//     origin: 'bottom',
+//     distance: '-100px'
+// });
+
+// sr.reveal('.img-user', {
+//     duration: 3000,
+// });
+
+// sr.reveal('.img2', {
+//     duration: 2000,
+//     origin: 'bottom',
+//     distance: '400px'
+// });
+
+// sr.reveal('.descripction', {
+//     duration: 2000,
+//     origin: 'bottom',
+//     distance: '400px'
+// });
+
+// sr.reveal('.cv', {
+//     duration: 2000,
+//     origin: 'bottom',
+//     distance: '400px'
+// });
+
+const toggleButton = document.getElementById("button-menu");
+const navWrapper = document.getElementById("nav");
+
+toggleButton.addEventListener("click", () => {
+  toggleButton.classList.toggle("close");
+  navWrapper.classList.toggle("show");
 });
 
-sr.reveal('.img-user', {
-    duration: 3000,
-});
-
-sr.reveal('.img2', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '400px'
-});
-
-sr.reveal('.descripction', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '400px'
-});
-
-sr.reveal('.cv', {
-    duration: 2000,
-    origin: 'bottom',
-    distance: '400px'
-});
-
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-  if(!menuOpen) {
-    menuBtn.classList.add('open');
-    menuOpen = true;
-  } else {
-    menuBtn.classList.remove('open');
-    menuOpen = false;
+navWrapper.addEventListener("click", (e) => {
+  if (e.target.id === "nav") {
+    navWrapper.classList.remove("show");
+    toggleButton.classList.remove("close");
   }
 });
 
-document.querySelector('.menu-btn').addEventListener('click', () => {
-  document.querySelector('.listOne').classList.toggle('show');
-})
+// const menuBtn = document.querySelector('.menu-btn');
+// let menuOpen = false;
+// menuBtn.addEventListener('click', () => {
+//   if(!menuOpen) {
+//     menuBtn.classList.add('open');
+//     menuOpen = true;
+//   } else {
+//     menuBtn.classList.remove('open');
+//     menuOpen = false;
+//   }
+// });
+
+// document.querySelector('.menu-btn').addEventListener('click', () => {
+//   document.querySelector('.listOne').classList.toggle('show');
+// })
