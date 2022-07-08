7a8acf50432849cb122a96ed066d4aa2f2f97914
Marco Antonio <marck.nova@gmail.com>
2022-07-07 23:57:59 -0500

    elimanacion de codigo js

--- JS/main.js
+++ JS/main.js
@@ -42,19 +42,3 @@ navWrapper.addEventListener("click", (e) => {
     toggleButton.classList.remove("close");
   }
 });
-
-// const menuBtn = document.querySelector('.menu-btn');
-// let menuOpen = false;
-// menuBtn.addEventListener('click', () => {
-//   if(!menuOpen) {
-//     menuBtn.classList.add('open');
-//     menuOpen = true;
-//   } else {
-//     menuBtn.classList.remove('open');
-//     menuOpen = false;
-//   }
-// });
-
-// document.querySelector('.menu-btn').addEventListener('click', () => {
-//   document.querySelector('.listOne').classList.toggle('show');
-// })
